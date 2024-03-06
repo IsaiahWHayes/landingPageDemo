f675e08d808327e9e1b8845b0da96778b4276788
Isaiah Hayes <isaiahwhayes@gmail.com>
2024-03-06 12:38:19 -0800

    updated code in the navBarLinks spec to improve execution speed by 7+ seconds

--- cypress/e2e/navBarLinks.cy.js
+++ cypress/e2e/navBarLinks.cy.js
@@ -10,32 +10,31 @@ describe('navigation bar links work correctly', () => {
 
   it ('navigates to the Java SDET Academy page', function () {
     // click the 'Java SDET Academy' button
-    cy.get('#menu-item-217931').click()
-
-    cy.url('pathname').should('equal', 'https://ultimateqa.ck.page/academy-coming-soon')
+    cy.get('#menu-item-217931').children()
+    .should('have.attr', 'href').and('equal', 'https://ultimateqa.ck.page/academy-coming-soon')
   })
 
   it ('navigates to the Testimonials page', function () {
     // click 'Success Stories' button
-    cy.get('#menu-item-217938').click()
-    cy.url('pathname').should('equal', 'https://ultimateqa.com/testimonials/')
+    cy.get('#menu-item-217938').children()
+    .should('have.attr', 'href').and('equal', 'https://ultimateqa.com/testimonials/')
   })
 
   it ('navigates to the Blog page', function () {
     // click the 'Blog' button
-    cy.get('#menu-item-218226').click()
-    cy.url('pathname').should('equal', 'https://ultimateqa.com/blog/')
+    cy.get('#menu-item-218226').children()
+    .should('have.attr', 'href').and('equal', 'https://ultimateqa.com/blog/')
   })
 
   it ('navigates to the About page', function () {
     // click the 'About' button
-    cy.get('#menu-item-217940').click()
-    cy.url('pathname').should('equal', 'https://ultimateqa.com/about/')
+    cy.get('#menu-item-217940').children()
+    .should('have.attr', 'href').and('equal', 'https://ultimateqa.com/about/')
   })
 
   it ('navigates to the ClickUp page', function () {
     // click the 'I Want a Free Discovery Session' button
-    cy.get('#menu-item-217945').click()
-    cy.url('pathname').should('contain', '/unlock-your-automation-potentialwitha-free-framework-assessment')
+    cy.get('#menu-item-217945').children()
+    .should('have.attr', 'href').and('contain', '/unlock-your-automation-potentialwitha-free-framework-assessment')
   })
 })
