45b42bdf5a28a6cad13c24d81adccfb20cc14bbf
Isaiah Hayes <isaiahwhayes@gmail.com>
2024-03-06 16:09:21 -0800

    added tests to validate the hrefs of social media links

--- cypress/e2e/footerLinks.cy.js
+++ cypress/e2e/footerLinks.cy.js
@@ -1,6 +1,6 @@
 /// <reference types = "cypress" />
 
-// before each test
+
 describe('footer links have the correct href\'s', function () {
     
     // visit the site
@@ -8,51 +8,81 @@ describe('footer links have the correct href\'s', function () {
         cy.visit('/')
     })
 
-    it('goes to the contact page', function () {
+    it('Contact page href is correct', function () {
         // contact button has the correct href
         cy.get('#menu-item-218100').children()
         .should('have.attr', 'href').and('equal', 'https://ultimateqa.com/contact/')
     })
 
-    it('goes to the Collection page', function () {
+    it('Collection page href is correct', function () {
         // collection button has the correct href
         cy.get('#menu-item-218104').children()
         .should('have.attr', 'href').and('equal', 'https://courses.ultimateqa.com/collections')
     })
 
-    it('goes to the Selenium Java page', function () {
+    it('Selenium Java page href', function () {
         // selenium java button has the correct href
         cy.get('#menu-item-218105').children()
         .should('have.attr', 'href').and('equal', 'https://academy.ultimateqa.com/java-sdet')
     })
 
-    it('goes to the Selenium C# page', function () {
+    it('Selenium C# page href is correct', function () {
         // selenium c# button has the correct href
         cy.get('#menu-item-218106').children()
         .should('have.attr', 'href').and('equal', 'https://courses.ultimateqa.com/courses/selenium-with-c')
     })
 
-    it('goes to the Selenium Resources page', function () {
+    it('Selenium Resources page href is correct', function () {
         // selenium resources button has the correct href
         cy.get('#menu-item-218107').children()
         .should('have.attr', 'href').and('equal', 'https://ultimateqa.com/best-selenium-webdriver-resources/')
     })
 
-    it('goes to the Automation Exercises page', function () {
+    it('Automation Exercises page href is correct', function () {
         // automation exercises button has the correct href
         cy.get('#menu-item-218108').children()
         .should('have.attr', 'href').and('equal', 'https://ultimateqa.com/automation/')
     })
 
-    it('goes to the Terms and Conditions page', function () {
+    it('Terms and Conditions page href is correct', function () {
         // terms and conditions button has the correct href
-        cy.get('.et-l--footer').contains('Terms and conditions')
+        cy.get('.et-l--footer').contains('Terms and conditions', { matchCase: false })
         .should('have.attr', 'href').and('equal', 'https://courses.ultimateqa.com/pages/terms')
     })
 
-    it('goes to the Automation Exercises page', function () {
-        // automation exercises button has the correct href
-        cy.get('.et-l--footer').contains('Privacy policy')
+    it('Privacy Policy page href is correct', function () {
+        // privacy policy button has the correct href
+        cy.get('.et-l--footer').contains('Privacy policy', { matchCase: false })
         .should('have.attr', 'href').and('equal', 'https://courses.ultimateqa.com/pages/privacy')
+    })
+
+    it('LinkedIn page href is correct', function () {
+        // linkedin button has the correct href
+        cy.get('.et-social-linkedin').children()
+        .should('have.attr', 'href').and('equal', 'https://www.linkedin.com/company/ultimate-qa')
+    })
+
+    it('Twitter page href is correct', function () {
+        // twitter button has the correct href
+        cy.get('.et-social-twitter').children()
+        .should('have.attr', 'href').and('equal', 'https://twitter.com/ultimateqahq')
+    })
+
+    it('Facebook page href is correct', function () {
+        // facebook button has the correct href
+        cy.get('.et-social-facebook').children()
+        .should('have.attr', 'href').and('equal', 'https://www.facebook.com/Ultimateqa1')
+    })
+
+    it('YouTube page href is correct', function () {
+        // facebook button has the correct href
+        cy.get('.et-social-youtube').children()
+        .should('have.attr', 'href').and('equal', 'https://www.youtube.com/@UltimateQA')
+    })
+
+    it('Instagram page href is correct', function () {
+        // facebook button has the correct href
+        cy.get('.et-social-instagram').children()
+        .should('have.attr', 'href').and('equal', 'https://www.instagram.com/ultimate.qa')
     })
 })
